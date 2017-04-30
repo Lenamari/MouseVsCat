8a3aa97343a592e60a7942b84f1bbf44a838388e
keellyp <keelly.p@gmail.com>
2017-04-30 14:35:08 +0200

    :rocket: Init gulp
    Signed-off-by: keellyp <keelly.p@gmail.com>

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,48 @@
-console.log('Yo');
+var container = document.querySelector('.container');
 
-var mouse = function(posY, posX, vitesse, couleur){
-  //propriétés
-  this.posY = posX;
-  this.posX = posY;
-  this.vitesse = vitesse;
-  this.couleur = couleur;
-  this.direction;
-  this.queue = new Array();
-  this.point;
-  this.enVie = true;
-  this.div;
+var canvas = function(width, height, color)
+{
+    this.width = width;
+    this.height = height;
+    this.color = color;
+
+    this.create = function()
+    {
+        this.div = document.createElement('div');
+        this.div.style.width = this.width + '%';
+        this.div.style.height = this.height + '%';
+        this.div.style.backgroundColor = this.color;
+
+        container.appendChild(this.div);
+    }
+}
+
+var canvas = new canvas (100, 100,"white");
+canvas.create();
+
+var mouse = function(posX, posY, speed, color)
+{
+    this.posX = posX;
+    this.posY = posY;
+    this.speed = speed;
+    this.color = color;
+
+    this.direction = new Array();
+    this.cross = new Array();
+    this.point;
+    this.isAlive = true;
+    this.div;
+
+    this.display = function()
+    {
+        this.div = document.createElement('div');
+        this.div = setAttribute('class', 'mouse');
+        this.div.style.top = this.posY + 'px';
+    }
+
+
+}
+
 
   //méthodes
   this.cross = new Array();

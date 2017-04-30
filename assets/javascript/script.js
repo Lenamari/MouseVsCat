247428b76f69c1e50391992448586d2391c19571
keellyp <keelly.p@gmail.com>
2017-04-30 16:11:08 +0200

    :sparkles: Create move function
    Signed-off-by: keellyp <keelly.p@gmail.com>

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,33 @@
 var container = document.querySelector('.container');
 
+// Create canvas object
 var canvas = function(width, height, color)
 {
+    // Proprieties
     this.width = width;
     this.height = height;
     this.color = color;
 
+    // Methods
     this.create = function()
     {
         this.div = document.createElement('div');
-        this.div.style.width = this.width + '%';
-        this.div.style.height = this.height + '%';
+        this.div.style.width = this.width + 'px';
+        this.div.style.height = this.height + 'px';
         this.div.style.backgroundColor = this.color;
-
+        this.div.classList.add('canvas');
         container.appendChild(this.div);
     }
 }
+// New canvas
+var canvas1 = new canvas (1200, 600,"white");
+canvas1.create();
 
-var canvas = new canvas (100, 100,"white");
-canvas.create();
 
+// Create mouse object
 var mouse = function(posX, posY, speed, color)
 {
+    // Proprieties
     this.posX = posX;
     this.posY = posY;
     this.speed = speed;
@@ -33,61 +39,41 @@ var mouse = function(posX, posY, speed, color)
     this.isAlive = true;
     this.div;
 
-    this.display = function()
+    // Methods
+    this.create = function()
     {
         this.div = document.createElement('div');
-        this.div = setAttribute('class', 'mouse');
+        this.div.classList.add('mouse');
         this.div.style.top = this.posY + 'px';
+        this.div.style.left = this.posX + 'px';
+        this.div.style.backgroundColor = this.color;
+
+        var canvas1 = container.querySelector('.canvas');
+        canvas1.appendChild(this.div);
     }
 
+    this.move = function()
+    {
+        var _this = this;
+        document.addEventListener('keydown', function(event)
+        {
+            if (event.keyCode == _this.cross[0])
+                _this.posY -= _this.speed;
+            else if (event.keyCode == _this.cross[1])
+                _this.posX += _this.speed;
+            else if (event.keyCode == _this.cross[2])
+                _this.posY += _this.speed;
+            else if (event.keyCode == _this.cross[3])
+                _this.posX -= _this.speed;
 
+            _this.div.style.left= _this.posX +"px";
+            _this.div.style.top = _this.posY +"px";
+
+        }, false);
+    }
 }
 
-
-  //méthodes
-  this.cross = new Array();
-  this.afficher = function(){
-      this.div = document.createElement("div");
-      this.div.setAttribute("class","mouse");
-      this.div.style.top=this.posY + "px";
-      this.div.style.left=this.posX + "px";
-      this.div.style.background=this.couleur;
-      var body = document.querySelector("body");
-      body.appendChild(this.div);
-  };
-
-  this.deplacer = function()
-  {
-    var _this = this;
-
-    window.addEventListener('keypress',function(e){
-      if (e.keyCode == _this.cross[0] )
-      {
-        _this.posY -= 10;
-      }
-      else if (e.keyCode == _this.cross[1])
-      {
-        _this.posX +=10;
-      }
-      else if (e.keyCode == _this.cross[2])
-      {
-        _this.posY += 10;
-      }
-      else if (e.keyCode == _this.cross[3])
-      {
-        _this.posX -= 10;
-      }
-      _this.div.style.top = _this.posY + "px";
-      _this.div.style.left = _this.posX + "px";
-
-    },false);
-  };
-}
-
-
-var mouse1 = new mouse (10, 20, 5, "red");
-
-mouse1.cross = [122,100,115,113];
-mouse1.deplacer();
-
-mouse1.afficher();
+var mouse1 = new mouse (0, 0, 20, "red");
+mouse1.cross = [38,39,40,37];
+mouse1.create();
+mouse1.move();

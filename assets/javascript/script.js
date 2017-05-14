676635fe9210e54671f5813c6dadcc50d7e54f45
keellyp <kelly.phan@hetic.net>
2017-05-14 21:40:25 +0200

    :rocket: Add features to game
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@ var container = document.querySelector('.container');
 // Create canvas object
 var canvas = function(width, height)
 {
-    // Proprieties
+    // Properties
     this.width = width;
     this.height = height;
 
@@ -24,19 +24,23 @@ canvas1.create();
 // Functions random
 function randomX() { return Math.abs(Math.floor(Math.random() * canvas1.width - 50)); }
 function randomY() { return Math.abs(Math.floor(Math.random() * canvas1.height - 50)); }
+function random(number) { return Math.abs(Math.floor(Math.random() * number) + 1); }
 
 
 // Create cheese object
-var cheese = function(width, height, posX, posY, size, addTime)
+var cheeseTotal = 5;
+var cheese = function(width, height, posX, posY, size, addTime, speed)
 {
-    // Proprieties
+    // Properties
     this.width = width;
     this.height = height;
     this.posX = posX;
     this.posY = posY;
     this.size = size;
     this.addTime = addTime;
-
+    this.dirX;
+    this.speed = speed;
+    this.div;
 
     // Methods
     this.create = function()
@@ -51,20 +55,46 @@ var cheese = function(width, height, posX, posY, size, addTime)
         var canvas = container.querySelector('.canvas');
         canvas.appendChild(this.div);
     }
+    this.direction= function()
+    {
+        var _this = this;
+
+        setInterval(function()
+        {
+            if (_this.posX <= 0)
+            {
+                console.log(_this.posX, _this.posY);
+                _this.posX = canvas1.width - _this.width;
+                _this.posY = randomY();
+            }
+            else
+            {
+                _this.posX -= _this.speed;
+                _this.div.style.left = _this.posX + 'px';
+                _this.div.style.top = _this.posY + 'px';
+
+            }
+        }, 30)
+    }
 }
 
-// Create cheeses on game
-var cheeses = [];
-for (var i = 0; i < 3; i++)
+// Create cheeses array
+function spoon()
 {
-    var cheeseSmall = new cheese (50, 50, randomX(), randomY(), 1, 10);
-    cheeseSmall.create();
-    cheeses.push(cheeseSmall);
+    var cheeses=[];
+    for (var i = 0; i < cheeseTotal; i++)
+    {
+        var cheese1 = new cheese(50,50,canvas1.width, randomY(), 1, 10, random(12) )
+        cheese1.create();
+        cheese1.direction();
+        cheeses.push(cheese1);
+    }
 }
+spoon()
 
 
 // Create mouse object
-var mouse = function(width, height, posX, posY, speed)
+var mouse = function(width, height, posX, posY, speed, dirX, dirY)
 {
     // Proprieties
     this.width = width;
@@ -72,18 +102,19 @@ var mouse = function(width, height, posX, posY, speed)
     this.posX = posX;
     this.posY = posY;
     this.speed = speed;
+    this.dirX = 0;
+    this.dirY = 0;
 
-    this.direction = new Array();
     this.cross = new Array();
-    this.point;
-    this.isAlive = true;
     this.div;
+
+    var isPressed = false;
 
     // Methods
     this.create = function()
     {
         this.div = document.createElement('div');
-        this.div.classList.add('mouse', 'dirRight');
+        this.div.classList.add('mouse', 'dir90');
         this.div.style.width = this.width + 'px';
         this.div.style.height = this.height + 'px';
         this.div.style.top = this.posY + 'px';
@@ -92,58 +123,117 @@ var mouse = function(width, height, posX, posY, speed)
         var canvas = container.querySelector('.canvas');
         canvas.appendChild(this.div);
     }
-
     this.move = function()
     {
         var _this = this;
+
         document.addEventListener('keydown', function(event)
         {
+
+            // if (event.keyCode == _this.cross[0] && _this.cross[1])
+            // {
+            //     _this.dirX = _this.speed;
+            //     _this.dirY = -_this.speed;
+            //     _this.div.className="mouse dir45";
+            // }
+            // else if (event.keyCode == _this.cross[1] && _this.cross[2])
+            // {
+            //     _this.dirX = _this.speed;
+            //     _this.dirY = _this.speed;
+            //     _this.div.className="mouse dir135";
+            // }
+            // else if (event.keyCode == _this.cross[2] && _this.cross[3])
+            // {
+            //     _this.dirX = -_this.speed;
+            //     _this.dirY = _this.speed;
+            //     _this.div.className="mouse dir225";
+            // }
+            // else if (event.keyCode == _this.cross[3] && _this.cross[4])
+            // {
+            //     _this.dirX = -_this.speed;
+            //     _this.dirY = -_this.speed;
+            //     _this.div.className = "mouse dir315";
+            // }
+            //
             if (event.keyCode == _this.cross[0])
             {
-                _this.posY -= _this.speed;
-                _this.div.className="mouse dirTop";
+                _this.dirY = -_this.speed;
+                _this.div.className="mouse dir0";
             }
-
             else if (event.keyCode == _this.cross[1])
             {
-                _this.posX += _this.speed;
-                _this.div.className="mouse dirRight";
+                _this.dirX = _this.speed;
+                _this.div.className="mouse dir90";
             }
             else if (event.keyCode == _this.cross[2])
             {
-                _this.posY += _this.speed;
-                _this.div.className="mouse dirBottom";
+                _this.dirY = _this.speed;
+                _this.div.className="mouse dir180";
             }
             else if (event.keyCode == _this.cross[3])
             {
-                _this.posX -= _this.speed;
-                _this.div.className = "mouse dirLeft";
+                _this.dirX = -_this.speed;
+                _this.div.className = "mouse dir270";
             }
 
-            _this.div.style.left= _this.posX +"px";
-            _this.div.style.top = _this.posY +"px";
+            if ( isPressed == false )
+            {
+                isPressed = true;
+                loop = setInterval( function()
+                {
+                    if (_this.posX <= 0)
+                    {
+                        _this.posX = 1;
+                    }
+                    else if (_this.posX >= canvas1.width-50) {
+                        _this.posX = canvas1.width-51;
+                    }
+                    else {
+                        _this.posX += _this.dirX;
+                    }
+                    if (_this.posY <= 0)
+                    {
+                        _this.posY = 1;
+                    }
+                    else if (_this.posY >= canvas1.height-100) {
+                        _this.posY = canvas1.height-101;
+                    }
+                    else {
+                        _this.posY += _this.dirY;
+                    }
+                    _this.div.style.left= _this.posX +"px";
+                    _this.div.style.top = _this.posY +"px";
 
-        collision(mouse1.width/2,mouse1.height/2);
+                }, 10 );
+            }
+            document.addEventListener('keyup', function(event)
+            {
+                isPressed = false;
+                _this.dirX = 0;
+                _this.dirY = 0;
+                clearInterval(loop);
+            }, false);
         }, false);
-
     }
 }
 
 // New mouse
-var mouse1 = new mouse (100, 100, 10, 20, 10);
+var mouse1 = new mouse (100, 100, 20, 20, 2, 0, 0);
 mouse1.cross = [38,39,40,37];
 
 mouse1.create();
 mouse1.move();
 
-// Collision function
-function collision(errorX, errorY)
-{
-    if (mouse1.posX - errorX <= cheeseSmall.posX &&
-        mouse1.posX + errorX >= cheeseSmall.posX &&
-        mouse1.posY - errorY <= cheeseSmall.posY &&
-        mouse1.posY + errorY >= cheeseSmall.posY)
-    {
-        alert('eat !')
-    }
-}
+
+//
+// // Collision function
+// function collision(errorX, errorY)
+// {
+//     if (mouse1.posX - errorX <= cheeseSmall.posX &&
+//         mouse1.posX + errorX >= cheeseSmall.posX &&
+//         mouse1.posY - errorY <= cheeseSmall.posY &&
+//         mouse1.posY + errorY >= cheeseSmall.posY)
+//     {
+//         alert('eat !')
+//     }
+// }
